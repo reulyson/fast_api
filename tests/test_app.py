dda349f16896148ddbc3f8e88e0c1efb9c8a4c4d
Reulyson Wanzeler <reulyson@gmail.com>
2026-02-05 19:50:19 -0400

    test: adiciona testes para criação e validação de tokens de acesso.

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,6 +3,7 @@
 from http import HTTPStatus
 
 from fast_api.schemas import UserPublic
+from fast_api.security import create_access_token
 
 
 def test_read_root(client) -> None:
@@ -185,3 +186,46 @@ def test_delete_user_error(client, mock_user) -> None:
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Not Found'}
+
+
+def test_create_token(client, mock_user):
+    response = client.post(
+        '/token/',
+        data={
+            'username': mock_user.email,
+            'password': mock_user.origin_password,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    access_token = create_access_token({'sub': mock_user.email})
+    assert response.json() == {
+        'access_token': access_token,
+        'token_type': 'Bearer',
+    }
+
+
+def test_create_token_error_username(client, mock_user):
+    response = client.post(
+        '/token/',
+        data={
+            'username': 'error@teste.com',
+            'password': mock_user.origin_password,
+        },
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Email ou senha incorretos'}
+
+
+def test_create_token_error_password(client, mock_user):
+    response = client.post(
+        '/token/',
+        data={
+            'username': mock_user.email,
+            'password': 'senha_error',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Senha incorreta'}
