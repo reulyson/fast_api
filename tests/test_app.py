10b379dbe63798c08ab6d5db28e3fbee89a33e5f
Reulyson Wanzeler <reulyson@gmail.com>
2026-02-06 12:01:05 -0400

    test: atualiza testes de criação, leitura, atualização e remoção de usuários para incluir autenticação com token.

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,26 +22,27 @@ def test_read_root(client) -> None:
     assert response.json() == {'message': 'Hello, World!'}
 
 
-def test_create_user(client) -> None:
+def test_create_user(client, token) -> None:
     """Testa se a rota de criação de usuário retorna o usuário criado."""
     response = client.post(
         '/users/',
         json={
-            'username': 'teste',
-            'email': 'teste@teste.com',
+            'username': 'novo_usuario',
+            'email': 'novo@teste.com',
             'password': '123456',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        'id': 1,
-        'username': 'teste',
-        'email': 'teste@teste.com',
+        'id': 2,
+        'username': 'novo_usuario',
+        'email': 'novo@teste.com',
     }
 
 
-def test_create_user_same_username(client, mock_user):
+def test_create_user_same_username(client, mock_user, token) -> None:
     """Testa se a rota de criação retorna erro quando username duplicado."""
     response = client.post(
         '/users/',
@@ -50,13 +51,14 @@ def test_create_user_same_username(client, mock_user):
             'email': 'teste@teste.com',
             'password': '123456',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {'detail': 'username já existe!'}
 
 
-def test_create_user_same_email(client, mock_user):
+def test_create_user_same_email(client, mock_user, token) -> None:
     """Testa se a rota de criação retorna erro quando email duplicado."""
     response = client.post(
         '/users/',
@@ -65,24 +67,31 @@ def test_create_user_same_email(client, mock_user):
             'email': 'teste@teste.com',
             'password': '123456',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {'detail': 'email já existe!'}
 
 
-def test_read_users(client, mock_user) -> None:
+def test_read_users(client, mock_user, token) -> None:
     """Testa se a rota de leitura de usuários retorna a lista de usuários."""
     user_schema = UserPublic.model_validate(mock_user).model_dump()
-    response = client.get('/users/')
+    response = client.get(
+        '/users/',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'users': [user_schema]}
 
 
-def test_read_user_id(client, mock_user) -> None:
+def test_read_user_id(client, mock_user, token) -> None:
     """Testa se a rota retorna o usuário pelo id."""
-    response = client.get(f'/users/{mock_user.id}')
+    response = client.get(
+        f'/users/{mock_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
@@ -92,23 +101,18 @@ def test_read_user_id(client, mock_user) -> None:
     }
 
 
-def test_read_user_id_error(client, mock_user) -> None:
+def test_read_user_id_error(client, mock_user, token) -> None:
     """Testa se a rota retorna 404 para usuário inexistente."""
-    response = client.get(f'/users/{(mock_user.id) + 1}')
+    response = client.get(
+        f'/users/{(mock_user.id) + 1}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
 
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json() == {'detail': 'Not Found'}
 
 
-def test_read_not_users(client):
-    """Testa se a rota de leitura de usuários retorna a lista de usuários."""
-    response = client.get('/users/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'users': []}
-
-
-def test_update_user(client, mock_user) -> None:
+def test_update_user(client, mock_user, token) -> None:
     """Testa se a rota de atualização retorna o usuário atualizado."""
     response = client.put(
         f'/users/{mock_user.id}',
@@ -117,6 +121,7 @@ def test_update_user(client, mock_user) -> None:
             'email': 'teste_novo@teste.com',
             'password': '654321',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     assert response.status_code == HTTPStatus.OK
@@ -127,7 +132,7 @@ def test_update_user(client, mock_user) -> None:
     }
 
 
-def test_update_user_same_fields(client, mock_user):
+def test_update_user_same_fields(client, mock_user, token) -> None:
     """Testa se atualizar usuário mantendo os mesmos campos é permitido."""
     # Cria um segundo usuário
     client.post(
@@ -137,6 +142,7 @@ def test_update_user_same_fields(client, mock_user):
             'email': 'outro@teste.com',
             'password': '123456',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     # Tentativa de atualizar
@@ -147,14 +153,15 @@ def test_update_user_same_fields(client, mock_user):
             'email': 'teste@teste.com',
             'password': '123456',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     assert response.status_code == HTTPStatus.CONFLICT
     assert response.json() == {'detail': 'username ou email já existem!'}
 
 
-def test_update_user_error(client, mock_user) -> None:
-    """Testa se a rota de atualização retorna 404 para usuário inexistente."""
+def test_update_user_error(client, mock_user, token) -> None:
+    """Testa se a rota retorna 403 ao tentar atualizar outro usuário."""
     response = client.put(
         f'/users/{(mock_user.id) + 1}',
         json={
@@ -162,30 +169,37 @@ def test_update_user_error(client, mock_user) -> None:
             'email': 'teste_novo@teste.com',
             'password': '654321',
         },
+        headers={'Authorization': f'Bearer {token}'},
     )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'Not Found'}
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {
+        'detail': 'Você não tem permissão para atualizar este usuário',
+    }
 
 
-def test_delete_user(client, mock_user) -> None:
+def test_delete_user(client, mock_user, token) -> None:
     """Testa se a rota de remoção retorna o usuário removido."""
     response = client.delete(
         f'/users/{mock_user.id}',
+        headers={'Authorization': f'Bearer {token}'},
     )
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User delete'}
 
 
-def test_delete_user_error(client, mock_user) -> None:
-    """Testa se a rota de remoção retorna 404 para usuário inexistente."""
+def test_delete_user_error(client, mock_user, token) -> None:
+    """Testa se a rota retorna 403 ao tentar remover outro usuário."""
     response = client.delete(
         f'/users/{(mock_user.id) + 1}',
+        headers={'Authorization': f'Bearer {token}'},
     )
 
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'Not Found'}
+    assert response.status_code == HTTPStatus.FORBIDDEN
+    assert response.json() == {
+        'detail': 'Você não tem permissão para remover este usuário',
+    }
 
 
 def test_create_token(client, mock_user):
